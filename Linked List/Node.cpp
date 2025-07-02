7261b68b06048354687076582dcae13794f75b11
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-07-02 11:59:32 +0530

    traversing in linked list

---
+++ Linked List/Node.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int size(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+int main()
+{
+    Node *a = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    Node *e = new Node(50);
+    Node *f = new Node(60);
+    Node *g = new Node(70);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+    e->next = f;
+    f->next = g;
+
+    display(a);
+    cout<<size(a);
+}
--- Linked List/creating-linked-list.cpp
+++ Linked List/creating-linked-list.cpp
@@ -9,7 +9,7 @@ public:
     Node(int val)
     {
         this->val = val;
-        this->next=NULL;
+        this->next = NULL;
     }
 };
 
@@ -17,13 +17,23 @@ int main()
 {
     // 10 20 30 40
     Node a(10);
-    Node b(30);
-    Node c(20);
+    Node b(20);
+    Node c(30);
     Node d(40);
 
     // forming ll
     a.next = &b;
     b.next = &c;
     c.next = &d;
-    
+
+    // cout << a.next->val;
+
+    Node temp = a;
+    while (true)
+    {
+        cout << temp.val << " ";
+        if (temp.next == NULL)
+            break;
+        temp = *(temp.next);
+    }
 }
